a80273664b35ad4208e6f791c16ee23e279921bb
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-11 22:35:10 +0100

    Owner Index Page and Controller. class mapping

--- pet-clinic-web/src/main/java/kkalinowski/springframework/kkpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/kkalinowski/springframework/kkpetclinic/controller/OwnerController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Krzysztof Kalinowski on 11/11/2019.
  */
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(){
         return "owners/index";
     }
