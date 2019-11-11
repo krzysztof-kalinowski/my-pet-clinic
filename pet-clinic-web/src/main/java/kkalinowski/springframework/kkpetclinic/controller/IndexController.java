31042335000ee0e1d19fd0d81fc394456a4a127e
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-11 21:57:10 +0100

    Index Page and Controller. Closes #7

---
+++ pet-clinic-web/src/main/java/kkalinowski/springframework/kkpetclinic/controller/IndexController.java
@@ -0,0 +1,17 @@
+package kkalinowski.springframework.kkpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Krzysztof Kalinowski on 11/11/2019.
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
