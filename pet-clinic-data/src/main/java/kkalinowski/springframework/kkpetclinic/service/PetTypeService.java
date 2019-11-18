62e459bca4de37af1c96843dad3db3baf3902a59
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-18 13:06:24 +0100

    PetTypeService & ServiceMap implementation. Closes #29

---
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/service/PetTypeService.java
@@ -0,0 +1,10 @@
+package kkalinowski.springframework.kkpetclinic.service;
+
+import kkalinowski.springframework.kkpetclinic.model.PetType;
+
+/**
+ * Created by Krzysztof Kalinowski on 18/11/2019.
+ */
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
