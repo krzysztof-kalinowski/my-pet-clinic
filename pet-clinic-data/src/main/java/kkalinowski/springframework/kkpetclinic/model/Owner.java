fd338baffa2bf69b49245c0f8cfe62d041642f4f
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-18 12:45:40 +0100

    Created Pet BaseEntity and updated Owner. Closes #26

--- pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package kkalinowski.springframework.kkpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Krzysztof Kalinowski on 10/11/2019.
  */
 
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
