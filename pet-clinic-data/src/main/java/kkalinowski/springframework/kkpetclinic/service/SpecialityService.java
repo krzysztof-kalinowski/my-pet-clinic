3c29cc28cc691d790968a324d411ef0e16206ba2
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-18 14:14:03 +0100

    Create Speciality Map Service implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/service/SpecialityService.java
@@ -0,0 +1,10 @@
+package kkalinowski.springframework.kkpetclinic.service;
+
+import kkalinowski.springframework.kkpetclinic.model.Speciality;
+
+/**
+ * Created by Krzysztof Kalinowski on 18/11/2019.
+ */
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
