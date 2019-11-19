93eaf49e46004f16c7c4d859c40ef2cb9cec5337
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-19 13:40:25 +0100

    Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/model/Visit.java
@@ -1,15 +1,24 @@
 package kkalinowski.springframework.kkpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Krzysztof Kalinowski on 18/11/2019.
  */
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
