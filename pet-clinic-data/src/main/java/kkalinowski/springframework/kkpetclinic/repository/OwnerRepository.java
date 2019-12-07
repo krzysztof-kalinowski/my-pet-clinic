dc091c7964aa01c129370bacad78d3678bce13d1
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-12-07 16:51:57 +0100

    Implement find Owner. Closes #54

--- pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/repository/OwnerRepository.java
@@ -3,6 +3,8 @@ package kkalinowski.springframework.kkpetclinic.repository;
 import kkalinowski.springframework.kkpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Krzysztof Kalinowski on 19/11/2019.
  */
@@ -10,4 +12,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/kkalinowski/springframework/kkpetclinic/service/OwnerService.java
@@ -2,6 +2,8 @@ package kkalinowski.springframework.kkpetclinic.service;
 
 import kkalinowski.springframework.kkpetclinic.model.Owner;
 
+import java.util.List;
+
 /**
  * Created by Krzysztof Kalinowski on 11/11/2019.
  */
@@ -10,4 +12,5 @@ public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
